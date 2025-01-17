14184f4a50b72b452a291274936275e1efbbe817
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-01-17 18:38:00 -0300

    Add new assets as mockup data

---
+++ constants/images.ts
@@ -0,0 +1,68 @@
+export const images = [
+  {
+    id: 1,
+    image: require('../assets/imgs/img-1.jpg')
+  },
+  {
+    id: 2,
+    image: require('../assets/imgs/img-2.jpg')
+  },
+  {
+    id: 3,
+    image: require('../assets/imgs/img-3.jpg')
+  }
+]
+
+export const reccommendations = [
+  {
+    id: 1,
+    name: 'La Grand Maison',
+    address: 'Champs-Elysees, Paris',
+    price: '$100.000',
+    image: require('../assets/imgs/img-4.jpg')
+  },
+  {
+    id: 2,
+    name: 'The Villa',
+    address: 'Champs-Elysees, Paris',
+    price: '$100.000',
+    image: require('../assets/imgs/img-5.jpg')
+  },
+  {
+    id: 3,
+    name: 'The Villa',
+    address: 'Champs-Elysees, Paris',
+    price: '$100.000',
+    image: require('../assets/imgs/img-6.jpg')
+  },
+  {
+    id: 4,
+    name: 'The Villa',
+    address: 'Champs-Elysees, Paris',
+    price: '$100.000',
+    image: require('../assets/imgs/img-7.jpg')
+  }
+]
+
+export const apartmentTypes = [
+  {
+    id: 1,
+    name: 'Apartamento'
+  },
+  {
+    id: 2,
+    name: 'Casa'
+  },
+  {
+    id: 3,
+    name: 'Casa de Condomínio'
+  },
+  {
+    id: 4,
+    name: 'Casa de Vila'
+  },
+  {
+    id: 5,
+    name: 'Otro'
+  }
+]

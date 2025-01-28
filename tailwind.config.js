ae544c13da22ec433ad8cee5b3cd73fd69583fe2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-01-27 23:34:29 -0300

    Add custom styles in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
   content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        opensans: ['open-sans-400', 'sans-serif'],
+        'opensans-bold': ['open-sans-700', 'sans-serif'],
+        'opensans-extrabold': ['open-sans-800', 'sans-serif'],
+        'opensans-semibold': ['open-sans-600', 'sans-serif'],
+        'opensans-medium': ['open-sans-500', 'sans-serif'],
+        'opensans-light': ['open-sans-300', 'sans-serif']
+      },
+      colors: {
+        primary: {
+          50: '#F9FCF3',
+          100: '#F4F9E7',
+          200: '#E9F2CE',
+          300: '#DEECB6',
+          400: '#D3E69E',
+          500: '#C8E087',
+          600: '#AED14D',
+          700: '#89AA2C',
+          800: '#5B711D',
+          900: '#2E390F',
+          950: '#171C07'
+        },
+        blackblue: {
+          100: '#020708',
+          200: '#010304',
+          300: '#000000'
+        }
+      }
+    }
   },
   plugins: []
 }
